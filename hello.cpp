bfc6c671a3c082d1616c53f7ee914650a02c5ba9
poojabhuse <pooja.bhuse@cumminscollege.in>
2014-08-05 08:56:58 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,13 @@ using namespace std;
 int main()
 {
 cout<<"Hello World!";
+cout<<"Good Morning!!!";
 return 0;
+}
+void add( )
+{
+cout<<"Enter the value of a,b ";
+cin>>a>>b;
+c=a+b;
+cout<<"Addition is"<<c;
 }
